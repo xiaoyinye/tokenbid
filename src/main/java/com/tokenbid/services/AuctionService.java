4873c87c4b6f88d23531e73d6cbc37d6e3fc5690
xiaoyinye <45239482+xiaoyinye@users.noreply.github.com>
2022-08-12 16:56:34 -0400

    send emails to buyer and seller

--- src/main/java/com/tokenbid/services/AuctionService.java
+++ src/main/java/com/tokenbid/services/AuctionService.java
@@ -1,7 +1,14 @@
 package com.tokenbid.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.tokenbid.models.Bid;
+import com.tokenbid.models.Item;
+import com.tokenbid.models.User;
+import com.tokenbid.repositories.BidRepository;
+import com.tokenbid.repositories.ItemRepository;
+import com.tokenbid.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +18,18 @@ import com.tokenbid.repositories.AuctionRepository;
 @Service
 public class AuctionService implements IService<Auction> {
     private AuctionRepository auctionRepository;
+    private EmailService emailService;
+    private ItemRepository itemRepository;
+    private BidRepository bidRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    public AuctionService(AuctionRepository auctionRepository) {
+    public AuctionService(AuctionRepository auctionRepository, EmailService emailService, ItemRepository itemRepository, BidRepository bidRepository, UserRepository userRepository) {
         this.auctionRepository = auctionRepository;
+        this.emailService = emailService;
+        this.itemRepository =itemRepository;
+        this.bidRepository = bidRepository;
+        this. userRepository = userRepository;
     }
 
     @Override
@@ -32,6 +47,13 @@ public class AuctionService implements IService<Auction> {
     @Override
     public void delete(int id) {
         if (auctionRepository.findById(id).isPresent()) {
+            Auction auction = auctionRepository.findById(id).get();
+            Item item = itemRepository.findById(auction.getItemId()).get();
+            Bid bid = bidRepository.findById(auction.getCurrentBid()).get();
+            User seller = userRepository.findById(item.getUserId()).get();
+            User buyer = userRepository.findById(bid.getUserId()).get();
+            emailService.sendEmail(seller.getEmail(), "Notification","Congratulations, your item " +item.getTitle() +" was successfully sold with " + auction.getCurrentBid() + "tokens");
+            emailService.sendEmail(buyer.getEmail(), "Notification","Congratulations, your bought " +item.getTitle() +" with " + auction.getCurrentBid() + "tokens");
             auctionRepository.deleteById(id);
         }
     }
