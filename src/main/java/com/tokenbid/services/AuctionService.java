1d00488bd6b3d86891cfeae478a7519d2070b78b
n8paternoster <n8paternoster@gmail.com>
2022-08-12 20:00:25 -0400

    Moved code to update function, added notify functions, changed dependencies to services

--- src/main/java/com/tokenbid/services/AuctionService.java
+++ src/main/java/com/tokenbid/services/AuctionService.java
@@ -1,14 +1,10 @@
 package com.tokenbid.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.tokenbid.models.Bid;
 import com.tokenbid.models.Item;
 import com.tokenbid.models.User;
-import com.tokenbid.repositories.BidRepository;
-import com.tokenbid.repositories.ItemRepository;
-import com.tokenbid.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,17 +15,17 @@ import com.tokenbid.repositories.AuctionRepository;
 public class AuctionService implements IService<Auction> {
     private AuctionRepository auctionRepository;
     private EmailService emailService;
-    private ItemRepository itemRepository;
-    private BidRepository bidRepository;
-    private UserRepository userRepository;
+    private ItemService itemService;
+    private BidService bidService;
+    private UserService userService;
 
     @Autowired
-    public AuctionService(AuctionRepository auctionRepository, EmailService emailService, ItemRepository itemRepository, BidRepository bidRepository, UserRepository userRepository) {
+    public AuctionService(AuctionRepository auctionRepository, EmailService emailService, ItemService itemService, BidService bidService, UserService userService) {
         this.auctionRepository = auctionRepository;
         this.emailService = emailService;
-        this.itemRepository =itemRepository;
-        this.bidRepository = bidRepository;
-        this. userRepository = userRepository;
+        this.itemService = itemService;
+        this.bidService = bidService;
+        this.userService = userService;
     }
 
     @Override
@@ -38,22 +34,30 @@ public class AuctionService implements IService<Auction> {
     }
 
     @Override
-    public void update(Auction item) {
-        if (auctionRepository.findById(item.getAuctionId()).isPresent()) {
-            auctionRepository.save(item);
+    public void update(Auction auction) {
+        if (auctionRepository.findById(auction.getAuctionId()).isPresent()) {
+            auctionRepository.save(auction);
+            Item item = itemService.getById(auction.getItemId());
+            User seller = userService.getById(item.getUserId());
+            switch (auction.getStatus()) {
+                case "Sold":
+                    Bid winningBid = bidService.getHighestBidForAnAuction(auction.getAuctionId());
+                    User buyer = userService.getById(winningBid.getUserId());
+                    notifySold(seller, buyer, item, winningBid);
+                    break;
+                case "Not Sold":
+                    notifyNotSold(seller, item);
+                    break;
+                case "Cancelled":
+                    // TODO : Email seller when they cancel an auction?
+                    break;
+            }
         }
     }
 
     @Override
     public void delete(int id) {
         if (auctionRepository.findById(id).isPresent()) {
-            Auction auction = auctionRepository.findById(id).get();
-            Item item = itemRepository.findById(auction.getItemId()).get();
-            Bid bid = bidRepository.findById(auction.getCurrentBid()).get();
-            User seller = userRepository.findById(item.getUserId()).get();
-            User buyer = userRepository.findById(bid.getUserId()).get();
-            emailService.sendEmail(seller.getEmail(), "Notification","Congratulations, your item " +item.getTitle() +" was successfully sold with " + auction.getCurrentBid() + "tokens");
-            emailService.sendEmail(buyer.getEmail(), "Notification","Congratulations, your bought " +item.getTitle() +" with " + auction.getCurrentBid() + "tokens");
             auctionRepository.deleteById(id);
         }
     }
@@ -72,7 +76,12 @@ public class AuctionService implements IService<Auction> {
         return auctionRepository.findAll();
     }
 
-    public void testMessage() {
-        System.out.println("AuctionService.testMessage()");
+    private void notifySold(User seller, User buyer, Item item, Bid winningBid) {
+        emailService.sendEmail(seller.getEmail(), "Notification","Congratulations, your item " + item.getTitle() + " was successfully sold for " + winningBid.getBid() + " tokens");
+        emailService.sendEmail(buyer.getEmail(), "Notification","Congratulations, you won the bid for " + item.getTitle() + " at " + winningBid.getBid() + " tokens");
     }
+    private void notifyNotSold(User seller, Item item) {
+        emailService.sendEmail(seller.getEmail(), "Notification", "Hi " + seller.getFirstName() + " " + seller.getLastName() + ",\nYour auction for item " + item.getTitle() + " has ended with no successful bids.");
+    }
+
 }
