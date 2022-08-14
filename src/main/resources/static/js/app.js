7d9992cf83fbfb2f6980d46d97756043a7851d7d
n8paternoster <n8paternoster@gmail.com>
2022-08-14 17:11:04 -0400

    update functions, utility functions to reduce code

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,12 +1,29 @@
-async function addUser(user) {
-    fetch(new Request('/auctions/add', {
+// Utility function to perform a POST request, returns a promise
+async function fetchPOST(path, body) {
+    return await fetch(new Request(path, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(user)
-    }))
-    .then((response) => response.json())
+        body: JSON.stringify(body)
+    }));
+}
+
+// Utility function to return a PUT request, returns a promise
+async function fetchPUT(path, body) {
+    return await fetch(new Request(path, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }));
+}
+
+// Add data to database
+async function addUser(user) {
+    fetchPOST('/users/add', user)
+    .then((response) => response || response.json())
     .then((data) => {
         console.log(data);
         // Display user added success message
@@ -16,16 +33,9 @@ async function addUser(user) {
         // Display user added failure message
     });
 }
-
 async function addItem(item) {
-    fetch(new Request('/items/add', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(item)
-    }))
-    .then((response) => response.json())
+    fetchPOST('/items/add', item)
+    .then((response) => response || response.json())
     .then((data) => {
         console.log(data);
         // Display item added success message
@@ -35,16 +45,9 @@ async function addItem(item) {
         // Display item added failure message
     });
 }
-
 async function addAuction(auction) {
-    fetch(new Request('/auctions/add', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(auction)
-    }))
-    .then((response) => response.json())
+    fetchPOST('/auctions/add', auction)
+    .then((response) => response || response.json())
     .then((data) => {
         console.log(data);
         // Display auction added success message
@@ -54,16 +57,9 @@ async function addAuction(auction) {
         // Display auction added failure message
     });
 }
-
 async function addBid(bid) {
-    fetch(new Request('/bids/add', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(bid)
-    }))
-    .then((response) => response.json())
+    fetchPOST('/bids/add', bid)
+    .then((response) => response || response.json())
     .then((data) => {
         console.log(data);
         // Display bid added success message
@@ -74,13 +70,69 @@ async function addBid(bid) {
     });
 }
 
+// TODO: Add image to S3 bucket
 async function addImage(image) {
-    // TODO add image to S3 bucket
+
 }
 
-//////////////////////////////////////////////////////////
-// Event listeners, TODO change when forms are added
-//////////////////////////////////////////////////////////
+// Update data on database
+async function updateUser(user) {
+    fetchPUT('/users/' + user.Id, user)
+    .then((response) => response || response.json())
+    .then((data) => {
+        console.log(data);
+        // Display user updated success message
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+        // Display user updated failure message
+    });
+}
+async function updateItem(item) {
+    fetchPUT('/items/' + item.Id, item)
+    .then((response) => response || response.json())
+    .then((data) => {
+        console.log(data);
+        // Display item updated success message
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+        // Display item updated failure message
+    });
+}
+async function updateAuction(auction) {
+    fetchPUT('/auctions/' + auction.Id, auction)
+    .then((response) => response || response.json())
+    .then((data) => {
+        console.log(data);
+        // Display auction updated success message
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+        // Display auction updated failure message
+    });
+}
+async function updateBid(bid) {
+    fetchPUT('/bids/' + bid.Id, bid)
+    .then((response) => response || response.json())
+    .then((data) => {
+        console.log(data);
+        // Display bid updated success message
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+        // Display bid updated failure message
+    });
+}
+
+// TODO: Update image on S3 bucket
+async function updateImage(image) {
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Event listeners, TODO change names when forms are added, add input validation
+/////////////////////////////////////////////////////////////////////////////
 
 document.getElementById('user-form').addEventListener('submit', async function(e) {
     e.preventDefault();
@@ -106,7 +158,7 @@ document.getElementById('item-form').addEventListener('submit', async function(e
     const data = new FormData(e.target);
     let image = data.get('image');
     let item = {};
-    item.userId = -1; // TODO: get userID from cache
+    item.userId = 1; // TODO: get userID from cache
     item.title = data.get('title');
     item.description = data.get('description');
     item.category = data.get('category');
@@ -132,6 +184,8 @@ document.getElementById('auction-form').addEventListener('submit', async functio
 
     // Add new auction to database
     await addAuction(auction);
+
+    // TODO: Start a timer if auction was successfully added
 });
 
 document.getElementById('bid-form').addEventListener('submit', async function(e) {
@@ -140,9 +194,12 @@ document.getElementById('bid-form').addEventListener('submit', async function(e)
     // Get the form values
     const data = new FormData(e.target);
     let bid = {};
-    bid.auctionId = -1; // TODO: get auctionId from URL
+    bid.userId = 1;     // TODO: get userId from cache
+    bid.auctionId = 7;  // TODO: get auctionId from URL
     bid.bid = data.get('bid');
 
     // Add new bid to database
     await addBid(bid);
+
+    // TODO: Check if the bid is high enough before submitting
 });
