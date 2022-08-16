5d2cadd208ce7f597318d7fc9a98f1ff05df39d1
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-08-15 23:14:55 -0400

    Login form added, and app.js modified

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -19,7 +19,7 @@
 // async function addImage(image) {
 //     let filePath = 'images/' + image.name;
 //     let fileUrl = 'https://' + bucketRegion + '.amazonaws.com/tokenbid/' + filePath;
-//     s3.upload({        
+//     s3.upload({
 //         Key: filePath,
 //         Body: file,
 //         ACL: 'public-read'
@@ -41,148 +41,178 @@
 
 // Utility function to perform an HTTPRequest, returns a promise with an HTTPResponse
 async function sendRequest(method, path, body) {
-    return await fetch(new Request(path, {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-    }));
+  return await fetch(
+    new Request(path, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+  );
 }
 
 // Add data to database
 async function addUser(user) {
-    return await sendRequest('POST', '/users/add', user);
+  return await sendRequest('POST', '/users/add', user);
+}
+async function loginUser(user) {
+  return await sendRequest('POST', '/users/login', user);
 }
 async function addItem(item) {
-    return await sendRequest('POST', 'items/add', item);
+  return await sendRequest('POST', '/items/add', item);
 }
 async function addAuction(auction) {
-    return await sendRequest('POST', '/auctions/add', auction);
+  return await sendRequest('POST', '/auctions/add', auction);
 }
 async function addBid(bid) {
-    return await sendRequest('POST', '/bids/add', bid);
+  return await sendRequest('POST', '/bids/add', bid);
 }
 
 // Update data on database
 async function updateUser(user) {
-    console.log(user);
-    return await sendRequest('PUT', '/users/' + user.userId, user);
+  console.log(user);
+  return await sendRequest('PUT', '/users/' + user.userId, user);
 }
 async function updateItem(item) {
-    return await sendRequest('PUT', '/items/' + item.itemId, item);
+  return await sendRequest('PUT', '/items/' + item.itemId, item);
 }
 async function updateAuction(auction) {
-    return await sendRequest('PUT', '/auctions/' + auction.auctionId, auction);
+  return await sendRequest('PUT', '/auctions/' + auction.auctionId, auction);
 }
 async function updateBid(bid) {
-    return await sendRequest('PUT', '/bids/' + bid.bidId, bid);
+  return await sendRequest('PUT', '/bids/' + bid.bidId, bid);
 }
 
 /////////////////////////////////////////////////////////////////////////////
 // Event listeners, TODO change names when forms are added, add input validation
 /////////////////////////////////////////////////////////////////////////////
 
-const userForm = document.getElementById('user-form');
-if (userForm) {
-    userForm.addEventListener('submit', async function(e) {
-        e.preventDefault();
+const registerForm = document.getElementById('register-form');
+if (registerForm) {
+  registerForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-        // Get the form values
-        const data = new FormData(e.target);
-        let user = {};
-        user.firstName = data.get('firstName');
-        user.lastName = data.get('lastName');
-        user.username = data.get('username');
-        user.email = data.get('email');
-        user.password = data.get('password');
-        user.tokens = 0;
+    // Get the form values
+    const data = new FormData(e.target);
+    let user = {};
+    user.firstName = data.get('firstName');
+    user.lastName = data.get('lastName');
+    user.username = data.get('username');
+    user.email = data.get('email');
+    user.password = data.get('password');
+    user.tokens = 0;
 
-        // Add new user to database
-        let response = await updateUser(user);
-        console.log(response);
-        if (response.ok) {
-            console.log("User added!");
-        } else {
-            console.log("Failed to add user");
-        }
-    });
+    // Add new user to database
+    let response = await addUser(user);
+    console.log(response);
+    if (response.ok) {
+      console.log('User added!');
+      window.location.href = '/login.html';
+    } else {
+      console.log('Failed to add user');
+    }
+  });
+}
+
+const loginForm = document.getElementById('login-form');
+if (loginForm) {
+  loginForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    // Get the form values
+    const data = new FormData(e.target);
+    let user = {};
+    user.username = data.get('username');
+    user.password = data.get('password');
+    console.log(user);
+
+    // Add new user to database
+    let response = await loginUser(user);
+    console.log(response);
+
+    if (response.ok) {
+      console.log('User logged in!');
+      window.location.href = '/explore.html';
+    } else {
+      console.log('Failed to log in user');
+    }
+  });
 }
 
 const itemForm = document.getElementById('item-form');
 if (itemForm) {
-    itemForm.addEventListener('submit', async function(e) {
-        e.preventDefault();
+  itemForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-        // Get the form values
-        const data = new FormData(e.target);
-        
-        let item = {};
-        item.userId = 1; // TODO: get userID from cache
-        item.title = data.get('title');
-        item.description = data.get('description');
-        item.category = data.get('category');
+    // Get the form values
+    const data = new FormData(e.target);
 
-        // Add new item to database and image to S3 bucket
-        let response = await addItem(item);
-        if (response.ok) {
-            console.log("Item added!");
-        } else {
-            console.log("Failed to add item");
-        }
+    let item = {};
+    item.userId = 1; // TODO: get userID from cache
+    item.title = data.get('title');
+    item.description = data.get('description');
+    item.category = data.get('category');
 
-        //let image = data.get('image');
-        //await addImage(image);
-    });
+    // Add new item to database and image to S3 bucket
+    let response = await addItem(item);
+    if (response.ok) {
+      console.log('Item added!');
+    } else {
+      console.log('Failed to add item');
+    }
+
+    //let image = data.get('image');
+    //await addImage(image);
+  });
 }
 
 const auctionForm = document.getElementById('auction-form');
 if (auctionForm) {
-    auctionForm.addEventListener('submit', async function(e) {
-        e.preventDefault();
+  auctionForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-        // Get the form values
-        const data = new FormData(e.target);
-        let hours = Number(data.get('length')); // auction length in hours
-        let start = new Date().getTime();
-        let end = start + (1000 * 60 * 60 * hours);
-        let auction = {};
-        auction.itemId = data.get('itemId');
-        auction.startingBid = data.get('startingBid');
-        auction.startTime = new Date(start).toJSON();
-        auction.endTime = new Date(end).toJSON();
+    // Get the form values
+    const data = new FormData(e.target);
+    let hours = Number(data.get('length')); // auction length in hours
+    let start = new Date().getTime();
+    let end = start + 1000 * 60 * 60 * hours;
+    let auction = {};
+    auction.itemId = data.get('itemId');
+    auction.startingBid = data.get('startingBid');
+    auction.startTime = new Date(start).toJSON();
+    auction.endTime = new Date(end).toJSON();
 
-        // Add new auction to database
-        let response = await addAuction(auction);
-        if (response.ok) {
-            console.log("Auction added!");
+    // Add new auction to database
+    let response = await addAuction(auction);
+    if (response.ok) {
+      console.log('Auction added!');
 
-            // TODO: Start a timer if auction was successfully added
-
-        } else {
-            console.log("Failed to add auction");
-        }
-    });
+      // TODO: Start a timer if auction was successfully added
+    } else {
+      console.log('Failed to add auction');
+    }
+  });
 }
 
 const bidForm = document.getElementById('bid-form');
 if (bidForm) {
-    bidForm.addEventListener('submit', async function(e) {
-        e.preventDefault();
+  bidForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-        // Get the form values
-        const data = new FormData(e.target);
-        let bid = {};
-        bid.userId = 1;     // TODO: get userId from cache
-        bid.auctionId = 7;  // TODO: get auctionId from URL
-        bid.bid = data.get('bid');
+    // Get the form values
+    const data = new FormData(e.target);
+    let bid = {};
+    bid.userId = 1; // TODO: get userId from cache
+    bid.auctionId = 7; // TODO: get auctionId from URL
+    bid.bid = data.get('bid');
 
-        // Add new bid to database
-        let response = await addBid(bid);
-        if (response.ok) {
-            console.log("Bid added!");
-        } else {
-            console.log("Failed to add bid");
-        }
-    });
+    // Add new bid to database
+    let response = await addBid(bid);
+    if (response.ok) {
+      console.log('Bid added!');
+    } else {
+      console.log('Failed to add bid');
+    }
+  });
 }
